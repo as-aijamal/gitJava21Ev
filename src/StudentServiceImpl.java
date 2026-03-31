f328198e12a0ed75b28aaf702b4518eb4a643e49
dron1x <usaamidin@gmail.com>
2026-03-31 16:39:19 +0600

    update

--- src/StudentServiceImpl.java
+++ src/StudentServiceImpl.java
@@ -1,11 +1,17 @@
 public class StudentServiceImpl implements StudentService{
+    private Group group = new Group();
     @Override
     public void saveStudent(Student student) {
-//realisation
     }
 
     @Override
     public Student getById(Long id) {
+        for (Student student : group.getList()) {
+            if (student.getAge() == id) {
+                return student;
+            }
+        }
         return null;
     }
+
 }
